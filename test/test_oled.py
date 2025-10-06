8c805b4c5febbb318041866c78738774ce1746fd
Enzo Gaggiotti <enzo.gaggiotti@epitech.eu>
2025-10-06 11:06:11 +0200

    [FIX] import SSD1306 test_oled.py

--- test/test_oled.py
+++ test/test_oled.py
@@ -1,10 +1,10 @@
 import board
 import busio
-from adafruit_ssd1306 import SSD1306_I2C
+from adafruit_ssd1306 import SSD1306
 from PIL import Image, ImageDraw, ImageFont
 
 i2c = busio.I2C(board.SCL, board.SDA)
-display = SSD1306_I2C(128, 64, i2c, addr=0x3C)
+display = SSD1306(128, 64, i2c, addr=0x3C)
 
 display.fill(0)
 display.show()

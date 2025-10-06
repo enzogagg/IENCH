3dd52ab0fbc2963c65fbc4f41a7ae3c654bdc0ae
Enzo Gaggiotti <enzo.gaggiotti@epitech.eu>
2025-10-06 11:16:49 +0200

    [FIX] use SSD1306 test_oled.py

--- test/test_oled.py
+++ test/test_oled.py
@@ -1,21 +1,26 @@
-from adafruit_ssd1306 import SSD1306
 import board
 import busio
+from adafruit_ssd1306 import SSD1306
 from PIL import Image, ImageDraw, ImageFont
 
-# I2C setup
+# Init I2C interface
 i2c = busio.I2C(board.SCL, board.SDA)
 
-# OLED display setup
-display = SSD1306(128, 64, i2c, addr=0x3C)
+# Init display
+display = SSD1306(width=128, height=64, i2c=i2c, addr=0x3C)
 
+# Clear display
 display.fill(0)
 display.show()
 
-# Create blank image for drawing.
+# Create image for drawing
 image = Image.new("1", (display.width, display.height))
 draw = ImageDraw.Draw(image)
+
+# Write text
 draw.text((10, 25), "Hello Pi!", fill=255)
 
+# Display image on screen
 display.image(image)
 display.show()
+
